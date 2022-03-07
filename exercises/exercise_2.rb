c499db82054c2890f64512c24e42f8a8f7500a31
Carter Hemington <carter.hemington@gmail.com>
2022-03-07 17:18:14 +0000

    exercise two completed

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+#loading/ retrieving the store and assigning to @store1
+
+@store2 = Store.find_by(id: 2)
+#loading/ retrieving the store and assigning to @store2
+
+@store1.update!(name: 'Burnaby11')
+# updating @store1 - changing name from 'Burnaby' to 'Burnaby11'
+
+
+
