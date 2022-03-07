173d5cd9539a6c14b10b1e0cdc6049fd1fc5978a
Carter Hemington <carter.hemington@gmail.com>
2022-03-07 16:45:57 +0000

    exercise 1 completed

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,22 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+Store.create!(name: 'Burnaby') do |s|
+  s.annual_revenue = 300000
+  s.mens_apparel = true
+  s.womens_apparel = true
+end
+
+Store.create!(name: 'Richmond') do |s|
+  s.annual_revenue = 1260000
+  s.mens_apparel = false
+  s.womens_apparel = true
+end
+
+Store.create!(name: 'Gastown') do |s|
+  s.annual_revenue = 190000
+  s.mens_apparel = true
+  s.womens_apparel = false
+end
+
+puts Store.count
